ee299f624d7fa8e76f521fa784f5516ca29c1ba1
Matthew Sumner <matt.m.sumner@gmail.com>
2013-11-22 10:42:30 -0700

    wrote a failing test for setting up the player two board

--- spec/features/start_new_game_spec.rb
+++ spec/features/start_new_game_spec.rb
@@ -5,6 +5,26 @@ describe "starting a new game" do
     visit root_path
     click_link "New Game"
 
+    select_ship_placements
+    click_button 'submit'
+
+    expect(page).to have_css "h1", text: "Player 2"
+  end
+
+  it "should create a 2nd player board after the 1st" do
+    visit root_path
+    click_link "New Game"
+
+    select_ship_placements
+    click_button 'submit'
+
+    select_ship_placements
+    click_button 'submit'
+
+    expect(page).to have_css "h1", text: "Player 1's turn"
+  end
+
+  def select_ship_placements
     select "A", from: 'board_aircraft_carrier_position_column'
     select "B", from: 'board_battleship_position_column'
     select "C", from: 'board_submarine_position_column'
@@ -22,9 +42,5 @@ describe "starting a new game" do
     select "right", from: 'board_submarine_orientation'
     select "right", from: 'board_cruiser_orientation'
     select "right", from: 'board_destroyer_orientation'
-
-    click_button 'submit'
-
-    expect(page).to have_css "h1", text: "Player 2"
   end
 end
